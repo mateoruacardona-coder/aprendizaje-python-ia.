d5efc9b31f65b17e539676d13d7e9a1418eea3ec
mateoruacardona-coder <mateoruacardona@gmail.com>
2026-01-15 05:11:32 -0500

    Create primeros_pasos.py

---
+++ primeros_pasos.py
@@ -0,0 +1,9 @@
+# Mi primera práctica de Python para IA
+usuario = "Mateo"
+progreso = "Iniciando nodo de Python"
+
+print(f"Hola, soy {usuario} y estoy en el camino de AI Engineer: {progreso}")
+
+# Ejemplo de lista (lo que usa la IA para procesar datos)
+temas_aprendidos = ["Sintaxis", "Variables", "Funciones"]
+print(f"Temas dominados hoy: {temas_aprendidos}")
